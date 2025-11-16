698b0d466ea5c0e61188cc9168f419ae6b05154e
Mohammed-SumairKhan <sumairn1432@gmail.com>
2025-11-16 14:10:39 +0530

    add a password util class that can hash the password before inserting the passwords into DB

---
+++ src/main/java/com/quizapp/util/PasswordUtil.java
@@ -0,0 +1,17 @@
+package com.quizapp.util;
+
+import java.security.MessageDigest;
+
+public class PasswordUtil {
+
+    // Method to hash password using SHA-256
+    public static String hashPassword(String password) throws Exception {
+        MessageDigest md = MessageDigest.getInstance("SHA-256");
+        byte[] hash = md.digest(password.getBytes());
+        StringBuilder sb = new StringBuilder();
+        for (byte b : hash) {
+            sb.append(String.format("%02x", b));
+        }
+        return sb.toString();
+    }
+}
